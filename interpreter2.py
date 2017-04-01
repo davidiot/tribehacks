6e74627c2f00b769627c6d50e0c989e76150b30a
Austin Liu <austin.by.liu@gmail.com>
2017-04-01 12:56:51 -0400

    Pipeline modification for spacy

--- interpreter2.py
+++ interpreter2.py
@@ -1,2 +1,36 @@
 import interpreter as interp
-f
+import spacy
+import spacy.symbols as sym
+from textpipeliner import PipelineEngine
+from textpipeliner.pipes import *
+
+nlp = spacy.load('en')
+
+def spacy_tokenization(text):
+    output = []
+    paragraphs = interp.tokenize_passage(text)
+
+    for paragraph in paragraphs:
+        doc = nlp(paragraph)
+        sentences = [sent.string.strip() for sent in doc.sents]
+        output.append(sentences)
+        return output
+
+tokenized_vals = spacy_tokenization(interp.load_file('input2.txt'))
+print(tokenized_vals)
+
+doc = nlp(tokenized_vals[0][0])
+
+pipes_structure = [SequencePipe([FindTokensPipe("VERB/nsubj/NNP"),
+                                 NamedEntityFilterPipe(),
+                                 NamedEntityExtractorPipe()]),
+                       AggregatePipe([FindTokensPipe("VERB"),
+                                      FindTokensPipe("VERB/xcomp/VERB/aux/*"),
+                                      FindTokensPipe("VERB/xcomp/VERB")]),
+                       AnyPipe([FindTokensPipe("VERB/[acomp,amod]/ADJ"),
+                                AggregatePipe([FindTokensPipe("VERB/[dobj,attr]/NOUN/det/DET"),
+                                               FindTokensPipe("VERB/[dobj,attr]/NOUN/[acomp,amod]/ADJ")])])
+                      ]
+
+engine = PipelineEngine(pipes_structure, doc, [0,1,2])
+engine.process()
