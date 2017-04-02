849b9318903c03b531fac92df178a4be331326a9
David Zhou <dz54@duke.edu>
2017-04-02 02:41:40 -0400

    now saves graphs again in main

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ text = interp.load_file()
 stripped_text = interp.strip_parens(text)
 tokenized_text = interp.lengthy_structured_tokenization(text)
 graphs = interp.get_dependency_graphs(tokenized_text)
-# for paragraph_array in graphs:
-#     for sentence_graph in paragraph_array:
-#         print(interp.sentence_from_graph(sentence_graph))
-#         print(interp.get_object_text_section(sentence_graph))
+for paragraph_array in graphs:
+    for sentence_graph in paragraph_array:
+        interp.save_dependency_graph(sentence_graph)
 maps = interp.analyze_verbs(graphs)
 interp.generate_p1(maps)
 interp.generate_p2(maps)
